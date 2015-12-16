469837d8a3ec39be5b0760f89afdee48943f10be
Christian Nagel <christian@christiannagel.com>
2015-12-16 12:44:45 +0100

    simple editor - commands

---
+++ MySimpleEditor/MySimpleEditor/MyCustomCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySimpleEditor
+{
+    public class MyCustomCommands
+    {
+        public static RoutedUICommand Action1 { get; } = new RoutedUICommand("Action1", "Action1", typeof(MyCustomCommands), new InputGestureCollection() { new KeyGesture(Key.B, ModifierKeys.Alt) });
+    }
+}

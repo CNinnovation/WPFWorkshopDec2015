b9dc4d684477c958525a433d8f366c58199bc124
Christian Nagel <christian@christiannagel.com>
2015-12-17 14:09:45 +0100

    horizontal alignment based on the type

---
+++ NumberInput/NumberInput/HorizontalConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NumberInput
+{
+    public class IsNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+             value is int || value is double;
+        
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HorizontalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int || value is double)
+            {
+                return HorizontalAlignment.Right;
+            }
+            else
+            {
+                return HorizontalAlignment.Left;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

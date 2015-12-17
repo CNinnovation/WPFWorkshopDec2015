5abba05d96650d9d1f8dd604ca37ffc4c1de8e02
Christian Nagel <christian@christiannagel.com>
2015-12-17 15:36:21 +0100

    long list sample

---
+++ LongListSample/LongListSample/MyDataFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongListSample
+{
+    public class MyDataFactory
+    {
+        public IEnumerable<MyData> ALongList()
+        {
+            Random r = new Random();
+            return Enumerable.Range(0, 10000).Select(x => new MyData { Text = $"sample {x}", Number = r.Next(400) }).ToList();
+        }
+    }
+}

b19e9153e2b2c453fb578cbe7fe84b819774c2ee
Christian Nagel <christian@christiannagel.com>
2015-12-17 11:33:11 +0100

    validation sample

---
+++ ValidationDemo/ValidationDemo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SomeData _someData;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _someData = new SomeData() { Val1 = 1, Val2 = 1 };
+            this.DataContext = _someData;
+        }
+
+        private void OnShowValue(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"val1: {_someData.Val1}, val2: {_someData.Val2}");
+        }
+    }
+}
---
+++ ValidationDemo/ValidationDemo/SomeData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationDemo
+{
+    public class SomeData : IDataErrorInfo
+    {
+        private int _val1;
+
+        string IDataErrorInfo.this[string columnName]
+        {
+            get
+            {
+                string retError = null;
+                switch (columnName)
+                {
+                    case nameof(Val2):
+                        if (Val2 > 50) retError = $"invalid value for {nameof(Val2)}";
+                        break;
+
+                    default:
+                        break;
+                }
+                return retError;
+            }
+        }
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Val1
+        {
+            get { return _val1; }
+            set
+            {
+                if (value > 50) throw new Exception("bad value!!");
+                _val1 = value;
+            }
+        }
+
+        public int Val2 { get; set; }
+
+
+    }
+}
